f7201dca1b21e6b5e197ee1dbce4d200b23c6a4f
team9-hcmus <75234933+team9-hcmus@users.noreply.github.com>
2020-12-08 13:37:30 +0700

    Update C.c

--- C.c
+++ C.c
@@ -2,11 +2,11 @@
 
 int main() {
     int year;
-    printf("Nhap nam: ");
+    printf("Nhap nam can kiem tra: ");
     scanf_s("%d",&year);
 
     if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-        printf("%d la mot nam nhuan", year);
+        printf("%d la nam nhuan", year);
     else
         printf("%d khong phai la nam nhuan", year);
 
