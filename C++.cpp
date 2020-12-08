22a34de305f556c544934f718909ebcde858b4b0
team9-hcmus <75234933+team9-hcmus@users.noreply.github.com>
2020-12-08 13:36:56 +0700

    Update C++.cpp

--- C++.cpp
+++ C++.cpp
@@ -4,20 +4,18 @@ using namespace std;
 int main()
 {
 	int n;
-	do {
-		cout << "Nhap nam: ";
-		cin >> n;
-	} while (n <= 0);
-	// Neu nam <= 0 thi chuong trinh se yeu cau nhap lai
+	cout << "Nhap nam can kiem tra: ";
+	cin >> n;
+	
 	if (n % 100 == 0)
 		if (n % 400 == 0)
 			cout << n << " la nam nhuan\n";
 		else
-			cout << n << " khong phai nam nhuan\n";
+			cout << n << " khong phai la nam nhuan\n";
 	else
 		if (n % 4 == 0)
 			cout << n << " la nam nhuan\n";
 		else
-			cout << n << " khong phai nam nhuan\n";
+			cout << n << " khong phai la nam nhuan\n";
 	return 0;
 }
